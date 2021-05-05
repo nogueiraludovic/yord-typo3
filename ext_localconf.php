b14206e4431cec356fcb3cf529078081f7900232
ludovic <ludovic@LAPLNOGUEIRA.home>
2021-05-05 08:58:23 +0200

    Update

---
+++ ext_localconf.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="DIR:EXT:yord/Configuration/TSconfig/Page">');
